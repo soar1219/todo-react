9240c24bd1b5975a48312d2a21e662b047491ec9
soar1219 <tsubasa1219.info@gmail.com>
2023-09-27 00:40:48 +0900

    stateを使ったクリックアクションの追加

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,25 @@ import React, { useState } from 'react';
 import "./style.css";
 
 export const App = () => {
+  const [todoText, setTodoText] = useState("");
   const [imcompleateTodos, setImcompleateTodos] = useState(["ああああ","いいいい"]);
   const [compleateTodos, setCompleateTodos] = useState(["ううううう"]);
+
+  const onChangeTodoText = (e) => {
+    setTodoText(e.target.value);
+  }
+
+  const onClickAdd = () => {
+    if (todoText === "") return;
+    const newTodos = [...imcompleateTodos, todoText]; /* 新しい配列を作成　ImcompleateTodosにtodoTextを追加 */
+    setImcompleateTodos(newTodos);/* newTodosをsetImcompleateTodosに反映する */
+    setTodoText("");
+  }
   return (
     <>
       <div className='input_area'>
-        <input placeholder='TODOを入力' />
-        <button>追加</button>
+        <input placeholder='TODOを入力' value={todoText} onChange={onChangeTodoText} />
+        <button onClick={onClickAdd}>追加</button>
       </div>
       <div className='incomplete_area'>
         <p className='title'>未完了のTODO</p>
