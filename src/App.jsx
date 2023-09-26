7f563cd7ee522fc71b0ac66ee8da83f5f1e219de
soar1219 <tsubasa1219.info@gmail.com>
2023-09-27 00:17:48 +0900

    今後を見据えたstateの実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./style.css";
 
 export const App = () => {
+  const [imcompleateTodos, setImcompleateTodos] = useState(["ああああ","いいいい"]);
+  const [compleateTodos, setCompleateTodos] = useState(["ううううう"]);
   return (
     <>
       <div className='input_area'>
@@ -10,26 +12,31 @@ export const App = () => {
       </div>
       <div className='incomplete_area'>
         <p className='title'>未完了のTODO</p>
+        {/* 状態が変化しそうなものは先にモックとしてstateで初期値を与えて処理する */}
         <ul>
-          <div className='list_row'>
-            <li>あああああ</li>
-            <button>完了</button>
-            <button>削除</button>
-          </div>
-          <div className='list_row'>
-            <li>あああああ</li>
-            <button>完了</button>
-            <button>削除</button>
-          </div>
+          {imcompleateTodos.map((todo) => {
+            return(
+              <div key={todo} className='list_row'>{/* ループの親タグに目標のためのキーを設定する必要がある mapで使用 */}
+                <li>{todo}</li>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            );
+          })};
         </ul>
       </div>
       <div className='complete_area'>
         <p className='title'>完了したTODO</p>
         <ul>
-          <div className='list_row'>
-            <li>ううううう</li>
-            <button>戻す</button>
-          </div>
+          {compleateTodos.map((todo) => {
+            return(
+              <div key={todo} className='list_row'>
+                <li>{todo}</li>
+                <button>戻す</button>
+              </div>
+            )
+          })}
+          
         </ul>
       </div>
     </>
