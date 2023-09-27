65f568095dc7aab528d5a14f71dda09c7222a59e
soar1219 <tsubasa1219.info@gmail.com>
2023-09-27 10:30:34 +0900

    削除ボタンの機能追加

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,14 @@ export const App = () => {
     setImcompleateTodos(newTodos);/* newTodosをsetImcompleateTodosに反映する */
     setTodoText("");
   }
+
+  const onClickDeleate = (index) => {
+    const newTodos = [...imcompleateTodos]
+    newTodos.splice(index, 1);
+    setImcompleateTodos(newTodos);
+  }
+
+
   return (
     <>
       <div className='input_area'>
@@ -26,12 +34,12 @@ export const App = () => {
         <p className='title'>未完了のTODO</p>
         {/* 状態が変化しそうなものは先にモックとしてstateで初期値を与えて処理する */}
         <ul>
-          {imcompleateTodos.map((todo) => {
+          {imcompleateTodos.map((todo , index) => { /* mapに２つ目の引数(index)を割り当てると順番が振り当てられる */
             return(
               <div key={todo} className='list_row'>{/* ループの親タグに目標のためのキーを設定する必要がある mapで使用 */}
                 <li>{todo}</li>
                 <button>完了</button>
-                <button>削除</button>
+                <button onClick={() => onClickDeleate(index)}>削除</button>{/* 関数にしておくと勝手に実行されてしまうのでアロー関数を設置することで勝手に関数が動かなくすることができる */}
               </div>
             );
           })};
