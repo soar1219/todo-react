fa752ab1a135c74be03e667c257c484e56070894
soar1219 <tsubasa1219.info@gmail.com>
2023-09-27 13:02:10 +0900

    CSSinJSで記載

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 
+const style = {
+    backgroundColor: "#c1ffff",
+    width: "400px",
+    height: "30px",
+    padding: "8px",
+    margin: "8px",
+    borderRadius: "8px"
+}
+
 export const InputTodo = (props) => {
     const { todoText, onChange, onClick } = props; /* propsの中身を使いやすいように取り出す */
     return (
         <>
-            <div className='input_area'>
+            <div style={style}>
                 <input placeholder='TODOを入力' value={todoText} onChange={onChange} />
                 <button onClick={onClick}>追加</button>
             </div>
