9ffc491fba8fa6e55fe793f3e7399a4520b52016
soar1219 <tsubasa1219.info@gmail.com>
2023-09-27 10:41:57 +0900

    タスクの完了機能

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,30 @@ export const App = () => {
 
   const onChangeTodoText = (e) => {
     setTodoText(e.target.value);
-  }
+  };
 
   const onClickAdd = () => {
     if (todoText === "") return;
     const newTodos = [...imcompleateTodos, todoText]; /* 新しい配列を作成　ImcompleateTodosにtodoTextを追加 */
     setImcompleateTodos(newTodos);/* newTodosをsetImcompleateTodosに反映する */
     setTodoText("");
-  }
+  };
 
   const onClickDeleate = (index) => {
     const newTodos = [...imcompleateTodos]
     newTodos.splice(index, 1);
     setImcompleateTodos(newTodos);
-  }
+  };
+
+  const onClickCompleate = (index) => {
+    const newIncompleateTodos = [...imcompleateTodos];
+    newIncompleateTodos.splice(index, 1);
+
+    const newCompleateTodos = [...compleateTodos, imcompleateTodos[index]];
+
+    setImcompleateTodos(newIncompleateTodos);
+    setCompleateTodos(newCompleateTodos);
+  };
 
 
   return (
@@ -38,11 +48,11 @@ export const App = () => {
             return(
               <div key={todo} className='list_row'>{/* ループの親タグに目標のためのキーを設定する必要がある mapで使用 */}
                 <li>{todo}</li>
-                <button>完了</button>
+                <button onClick={() => onClickCompleate(index)}>完了</button>
                 <button onClick={() => onClickDeleate(index)}>削除</button>{/* 関数にしておくと勝手に実行されてしまうのでアロー関数を設置することで勝手に関数が動かなくすることができる */}
               </div>
-            );
-          })};
+            )
+          })}
         </ul>
       </div>
       <div className='complete_area'>
